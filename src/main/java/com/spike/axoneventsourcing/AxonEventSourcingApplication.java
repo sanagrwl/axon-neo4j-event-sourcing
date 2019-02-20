ed620524c526b8dadc4087a5dfde23f88e29f0ba
Sandeep Aggarwal <1342667+sanagrwl@users.noreply.github.com>
2019-02-20 13:49:26 -0600

    initial commit

---
+++ src/main/java/com/spike/axoneventsourcing/AxonEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.spike.axoneventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonEventSourcingApplication.class, args);
+	}
+
+}
